18d66f3f1cfdc874aa2f86ef4d9895c7b2db266c
Kron1749 <drakgr98@gmail.com>
2022-09-02 16:53:56 +0300

    Created Deposit function to lendingPool

---
+++ scripts/aaveBorrow.js
@@ -0,0 +1,43 @@
+const { getWeth, AMOUNT } = require("../scripts/getWeth")
+const { getNamedAccounts, ethers } = require("hardhat")
+
+async function main() {
+    await getWeth()
+    const { deployer } = await getNamedAccounts()
+    const lendingPool = await getLendingPool(deployer)
+    console.log(`LengingPool address ${lendingPool.address}`)
+
+    //deposit
+    const wethTokenAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+    //approve
+    await approveERC20(wethTokenAddress, lendingPool.address, AMOUNT, deployer)
+    console.log("Depositing...")
+    await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0)
+    console.log("Deposited...")
+}
+
+async function getLendingPool(account) {
+    const { deployer } = await getNamedAccounts()
+    const lendingPoolAddressProvider = await ethers.getContractAt(
+        "ILendingPoolAddressesProvider",
+        "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
+        account
+    )
+    const lendingPoolAddress = await lendingPoolAddressProvider.getLendingPool()
+    const lendingPool = await ethers.getContractAt("ILendingPool", lendingPoolAddress, account)
+    return lendingPool
+}
+
+async function approveERC20(erc20Address, spenderAddress, amountToSpend, account) {
+    const erc20Token = await ethers.getContractAt("IERC20", erc20Address, account)
+    const tx = await erc20Token.approve(spenderAddress, amountToSpend)
+    await tx.wait(1)
+    console.log("Approved")
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error)
+        process.exit(1)
+    })
